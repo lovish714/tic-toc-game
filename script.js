2888b3b27c18b86b3f8cb7b8efba0deab0fc583e
lovish714 <lovishtalwar07@gmail.com>
2024-10-04 13:32:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,67 +1,81 @@
 console.log("Welcome to tic tac toe");
-let music = new Audio("music.mp3")
-let audioTurn = new Audio("ting.mp3")
-let gameover = new Audio("gameover.mp3")
-let turn = "X"
-let isgameover = false
+let music = new Audio("music.mp3");
+let audioTurn = new Audio("ting.mp3");
+let gameover = new Audio("gameover.mp3");
+let turn = "X";
+let isgameover = false;
 
-//function to change the turn
+// Function to change the turn
 const changeTurn = () => {
-    return turn === "X" ? "0" : "X"
-}
+    return turn === "X" ? "O" : "X";
+};
 
-//function to check for a win 
+// Function to check for a win 
 const checkWin = () => {
-    let boxtext = document.getElementsByClassName("boxtext")
+    let boxtext = document.getElementsByClassName("boxtext");
     let wins = [
-        [0, 1, 2 ,5,5,0],
-        [3, 4, 5 ,5,15,0],
-        [6, 7, 8 ,5,25,0],
-        [0, 3, 6 ,-5,15,90],
-        [1, 4, 7 ,5,15,90],
-        [2, 5, 8 ,15,15,90],
-        [0, 4, 8 ,5,15,45],
-        [2, 4, 6 ,5,15,135],
-    ]
+        [0, 1, 2, 5, 5, 0],
+        [3, 4, 5, 5, 15, 0],
+        [6, 7, 8, 5, 25, 0],
+        [0, 3, 6, -5, 15, 90],
+        [1, 4, 7, 5, 15, 90],
+        [2, 5, 8, 15, 15, 90],
+        [0, 4, 8, 5, 15, 45],
+        [2, 4, 6, 5, 15, 135],
+    ];
     wins.forEach(e => {
         if ((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "")) {
-            document.querySelector(".info").innerText = boxtext[e[0]].innerText + "Won"
+            document.querySelector(".info").innerText = boxtext[e[0]].innerText + " Won";
             isgameover = true;
-            gameover.play()
-            document.querySelector(".imgbox").getElementsByTagName("img")[0].style.width = "200px"
-            document.querySelector(".line").style.width= "20vw"
-            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`
-        } 
-    })
-}
+            gameover.play();
+            document.querySelector(".imgbox").getElementsByTagName("img")[0].style.width = "200px";
+            document.querySelector(".line").style.width = "20vw";
+            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
+        }
+    });
+};
 
-//game logic
-music.play()
-let boxes = document.getElementsByClassName("box")
+// Function to check for a draw
+const checkDraw = () => {
+    let boxtext = document.getElementsByClassName("boxtext");
+    // Check if all boxes are filled
+    let allFilled = Array.from(boxtext).every(box => box.innerText !== "");
+    
+    if (allFilled && !isgameover) {
+        document.querySelector(".info").innerText = "It's a draw!";
+        isgameover = true; // Set game over flag
+        gameover.play(); // Optionally play a game over sound
+    }
+};
+
+// Game logic
+music.play();
+let boxes = document.getElementsByClassName("box");
 Array.from(boxes).forEach(element => {
-    let boxtext = element.querySelector('.boxtext')
+    let boxtext = element.querySelector('.boxtext');
     element.addEventListener("click", () => {
-        if (boxtext.innerText === '') {
+        if (boxtext.innerText === '' && !isgameover) {
             boxtext.innerText = turn;
-            turn = changeTurn()
-            audioTurn.play()
-            checkWin()
+            audioTurn.play();
+            checkWin();
+            checkDraw(); // Check for draw after checking win
             if (!isgameover) {
+                turn = changeTurn();
                 document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
             }
         }
-    })
-})
+    });
+});
 
- //add an event listener to reset button
- reset.addEventListener("click",()=>{
-    let boxtext = document.querySelectorAll(".boxtext")
-    Array.from(boxtext).forEach(element=>{
-        element.innerText = ""
-    })
-    turn = "X"
-    isgameover = false
-    document.getElementsByClassName("info")[0].innerText = "Turn for" + turn
-     document.querySelector(".imgbox").getElementsByTagName("img")[0].style.width = "0px"
-      document.querySelector(".line").style.width= "0vw"
- })
+// Add an event listener to reset button
+reset.addEventListener("click", () => {
+    let boxtext = document.querySelectorAll(".boxtext");
+    Array.from(boxtext).forEach(element => {
+        element.innerText = "";
+    });
+    turn = "X";
+    isgameover = false;
+    document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+    document.querySelector(".imgbox").getElementsByTagName("img")[0].style.width = "0px";
+    document.querySelector(".line").style.width = "0vw";
+});
